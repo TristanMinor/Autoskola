6ca50c8184e628fbdcab6cde40d06cbbb4d5d772
TristanMinor <minor.marek@gmail.com>
2015-03-19 19:29:48 +0100

    Explaining modal

--- www/js/questions-signs-question-controller.js
+++ www/js/questions-signs-question-controller.js
@@ -1,5 +1,5 @@
 angular.module('autoskola')
-  .controller('QuestionsSignsQuestionController', function($scope, $http, $state) {
+  .controller('QuestionsSignsQuestionController', function($scope, $http, $state, $ionicModal) {
     $scope.data = [];
 
     // load json from file
@@ -17,11 +17,32 @@ angular.module('autoskola')
             question = qstn;
           }
         });
-        
+
       });
 
       $scope.question = question;
 
+    });
+
+    // Load the modal from the given template URL
+    $ionicModal.fromTemplateUrl('templates/explaining.html', function($ionicModal) {
+      $scope.modal = $ionicModal;
+    }, {
+      // Use the scope for the scope of the modal to keep it simple
+      scope: $scope,
+      // The animation
+      animation: 'slide-in-up'
+    });
+
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
     });
 
   });
