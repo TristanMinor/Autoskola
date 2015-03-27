6747adb654a12e6d70ad65248cae662cee5e9bf4
TristanMinor <minor.marek@gmail.com>
2015-03-27 20:47:52 +0100

    shaking device – first attempt

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -27,6 +27,27 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/uk.co.ilee.shake/www/shake.js",
+        "id": "uk.co.ilee.shake.Shake",
+        "clobbers": [
+            "shake"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -34,7 +55,9 @@ module.exports.metadata =
 {
     "com.ionic.keyboard": "1.0.4",
     "org.apache.cordova.console": "0.2.13",
-    "org.apache.cordova.device": "0.3.0"
+    "org.apache.cordova.device": "0.3.0",
+    "uk.co.ilee.shake": "0.3.1",
+    "org.apache.cordova.device-motion": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
