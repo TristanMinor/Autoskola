3074efa3f712a8309fff8f97050a23a3da2d1ac6
TristanMinor <minor.marek@gmail.com>
2015-04-26 21:56:17 +0200

    Rewards

--- www/js/overview-controller.js
+++ www/js/overview-controller.js
@@ -1,9 +1,10 @@
 angular.module('autoskola')
-  .controller('OverviewController', function($scope, $http, OverviewService) {
+  .controller('OverviewController', function($scope, $http, OverviewService, $ionicModal) {
 
     $scope.rewards = [];
     $scope.localData = {};
     $scope.tabName = 'tab-progres';
+    $scope.selectedReward = {};
 
     $scope.setTab = function(tab) {
     	$scope.tabName = tab;
@@ -17,5 +18,29 @@ angular.module('autoskola')
     $scope.unlockReward = function(reward) {
       OverviewService.unlockReward(reward);
     }
+
+    // Load the modal from the given template URL
+    $ionicModal.fromTemplateUrl('templates/modals/modal-reward.html', function($ionicModal) {
+      $scope.modal = $ionicModal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up',
+    });
+
+    $scope.openRewardModal = function(reward) {
+      $scope.selectedReward = reward;
+      $scope.modal.show();
+    };
+
+    $scope.closeRewardModal = function() {
+      $scope.modal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+      $scope.chapter.remove();
+      $scope.content.remove();
+    });
 
   });
