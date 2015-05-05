a398c18eb1d37c54b0fb179602aa93ff431c3d06
TristanMinor <minor.marek@gmail.com>
2015-05-05 19:22:34 +0200

    stuff

---
+++ platforms/ios/www/js/questions-notice-question-controller.js
@@ -0,0 +1,31 @@
+angular.module('autoskola')
+  .controller('QuestionsNoticeQuestionController', function($scope, $state, $ionicModal, NoticeService, TheoryService) {
+
+    $scope.localData = {};
+
+    $scope.optionsModel = ["a","b","c"];
+    $scope.localData = NoticeService.get().localData;
+    $scope.question = NoticeService.getQuestion($state.params.id);
+    $scope.explainings = TheoryService.getNoticeExplanation($state.params.id);
+
+    $scope.pinQuestion = function(item) {
+      NoticeService.pinQuestion(item);
+    }
+
+    $ionicModal.fromTemplateUrl('templates/modals/modal-explaining.html', function($ionicModal) {
+      $scope.modal = $ionicModal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+
+  });
